6df727add3539b3e737347a26804ef4c82c20d7d
jmser <jserenari@protonmail.com>
2022-10-17 18:28:12 -0400

    Added directory structure

--- src/main/java/com/badhand/suitup/SuitUp.java
+++ src/main/java/com/badhand/suitup/SuitUp.java
@@ -1,9 +1,7 @@
 package com.badhand.suitup;
 
-public class SuitUp 
-{
-    public static void main( String[] args )
-    {
+public class SuitUp {
+    public static void main(String[] args) {
        
     }
 }
