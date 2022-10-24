d4c855b1022fb8ba5207e76cc10528e851327637
ColeEichner <COE13@pitt.edu>
2022-10-24 17:41:42 -0400

    state added
    Added state enum

--- src/main/java/com/badhand/suitup/game/Game.java
+++ src/main/java/com/badhand/suitup/game/Game.java
@@ -1,4 +1,11 @@
 package com.badhand.suitup.game;
+import com.badhand.suitup.ui.*;
+
+public final enum state {
+    MENU_SETTINGS,
+    MENU_MAIN,
+    MENU_LEVEL_SELECT,
+}
 
 public class Game {
     
