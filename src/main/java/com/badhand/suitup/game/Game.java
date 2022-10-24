812fb00c0dec8baa5a161544b5a92b511be2cd61
ColeEichner <COE13@pitt.edu>
2022-10-24 17:51:32 -0400

    Added Scene
    Added Scene interface

--- src/main/java/com/badhand/suitup/game/Game.java
+++ src/main/java/com/badhand/suitup/game/Game.java
@@ -1,12 +1,12 @@
 package com.badhand.suitup.game;
 import com.badhand.suitup.ui.*;
 
-public final enum state {
-    MENU_SETTINGS,
-    MENU_MAIN,
-    MENU_LEVEL_SELECT,
-}
-
 public class Game {
     
+    public enum state {
+        MENU_SETTINGS,
+        MENU_MAIN,
+        MENU_LEVEL_SELECT,
+    }
 }
+
