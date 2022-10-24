134f75fdf2b3d38f3738e91859bbe6d8d5e86d72
Anderis-Dobrowolski <and209@pitt.edu>
2022-10-24 18:53:17 -0400

    Created EventManager setup
    Created Event.java and Events.java files. Modified the EventManager to create the actual manager.

--- src/main/java/com/badhand/suitup/events/EventManager.java
+++ src/main/java/com/badhand/suitup/events/EventManager.java
@@ -1,5 +1,25 @@
 package com.badhand.suitup.events;
 
+import java.util.*;
+
 public class EventManager {
-    
+    private LinkedList<Event> eventQueue = LinkedList<Event>();
+
+
+    private static EventManager instance = null;
+
+    private EventManager(){}; // Singleton
+
+
+    public static EventManager getInstance(){
+        if(instance == null) instance = new EventManager();
+        return instance;
+    }
+
+    public Event pop() {
+        return eventQueue.pop();
+    }
+    public void push(Event e) {
+        eventQueue.push(e);
+    }
 }
