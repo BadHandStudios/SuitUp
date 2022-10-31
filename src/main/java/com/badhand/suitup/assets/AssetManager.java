83337bd2325b4a0428d8e021f4d91012ac471a58
ColeEichner <COE13@pitt.edu>
2022-10-31 02:05:21 -0400

    AssetManager Created
    Created AssetManager and FilePlayer (MP3 Player) classes. I also added the sounds and images folders with test files in each.

--- src/main/java/com/badhand/suitup/assets/AssetManager.java
+++ src/main/java/com/badhand/suitup/assets/AssetManager.java
@@ -1,5 +1,60 @@
 package com.badhand.suitup.assets;
 
+import java.util.*;
+import java.io.*;
+
 public class AssetManager {
+    private static AssetManager single_instance = null;
+    private HashMap<String, File> images = null;
+    private HashMap<String, File> sounds = null;
+    private FilePlayer player;
+
+    private AssetManager() {
+        File dir = new File( "src\\main\\java\\com\\badhand\\suitup\\assets\\Resources\\images");
+        File[] files = dir.listFiles();
+
+        images = new HashMap<>(files.length + 1, 1);
+
+        for (int i = 0; i < files.length; i++) {
+            String hold = files[i].getName();
+            if(hold != null && hold.contains(".")){
+                hold.substring(0, hold.lastIndexOf('.'));
+            }
+            images.put(hold, files[i]);
+        }
+
+        dir = new File( "src\\main\\java\\com\\badhand\\suitup\\assets\\Resources\\sounds");
+        files = dir.listFiles();
+
+        sounds = new HashMap<>(files.length + 1, 1);
+
+        for (int i = 0; i < files.length; i++) {
+            String hold = files[i].getName();
+            if(hold != null && hold.contains(".")){
+                hold.substring(0, hold.lastIndexOf('.'));
+            }
+            sounds.put(hold, files[i]);
+        }
+    }
+
+    public static AssetManager getInstance() {
+        if (single_instance == null) {
+            single_instance = new AssetManager();
+        }
+        return single_instance; 
+    }
+
+    public File getImage(String fileName) {
+        return images.get(fileName);
+    }
+
+    public void playSound(String fileName) {
+        player = new FilePlayer(sounds.get(fileName));
+        player.play();
+    }
+
+    public void stopSound() {
+        player.close();
+    }
     
 }
--- src/main/java/com/badhand/suitup/game/Scenes/MenuMain.java
+++ src/main/java/com/badhand/suitup/game/Scenes/MenuMain.java
@@ -1,5 +1,6 @@
 package com.badhand.suitup.game.Scenes;
 
+
 public class MenuMain {
     
 }
